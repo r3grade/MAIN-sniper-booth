4b1e37d8bbb97c02888783b97a63f95571e3a726
r3grade <154843126+r3grade@users.noreply.github.com>
2023-12-25 19:23:09 -0600

    Update freesniper.lua

--- freesniper.lua
+++ freesniper.lua
@@ -94,7 +94,7 @@ end
 local function checklisting(uid, gems, item, version, shiny, amount, username, playerid)
     gems = tonumber(gems)
 
-    if string.find(item, "Huge") and gems <= 1000000 then
+    if string.find(item, "Huge") and gems <= 300000 then
         game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
         processListingInfo(uid, gems, item, version, shiny, amount, username)
     elseif gems <= 5 then
