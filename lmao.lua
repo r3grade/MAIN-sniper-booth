883af9be73e16c3f67b1f44a08045a7397cec522
r3grade <154843126+r3grade@users.noreply.github.com>
2023-12-31 14:06:38 -0600

    Update lmao.lua

--- lmao.lua
+++ lmao.lua
@@ -2,7 +2,7 @@ getgenv().webhook = "https://discord.com/api/webhooks/1191054888449351740/w7d5gy
 getgenv().webhookFail = "https://discord.com/api/webhooks/1191054888449351740/w7d5gydr2XrxHoAxzOtUioVWvIMtgEv6NmkrOzBuDoyTWNiDslw4kRSPne0N_wwFCJlB" 
 getgenv().userid = "1047637526099279873"
 getgenv().alts = {"Dexalts_mbhrx","Dexalts_9in9e", “Dexalts_a61ng”, “Dexalts_fss4n”, “Dexalts_jhhif”} 
-getgenv().normalwebhook = https://discord.com/api/webhooks/1191054888449351740/w7d5gydr2XrxHoAxzOtUioVWvIMtgEv6NmkrOzBuDoyTWNiDslw4kRSPne0N_wwFCJlB"
+getgenv().normalwebhook = "https://discord.com/api/webhooks/1191054888449351740/w7d5gydr2XrxHoAxzOtUioVWvIMtgEv6NmkrOzBuDoyTWNiDslw4kRSPne0N_wwFCJlB"
 getgenv().snipeNormalPets = false 
 repeat wait() until game:IsLoaded()
 if game.PlaceId == 15502339080 then
